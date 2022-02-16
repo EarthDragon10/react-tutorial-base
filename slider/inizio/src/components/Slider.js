9df59cfba507385f8aa4c618f1f8268d0d5c39ac
Laguda Stefano <lagudadev@gmail.com>
2022-02-16 10:41:52 +0100

    feat: beginned slider project

--- slider/inizio/src/components/Slider.js
+++ slider/inizio/src/components/Slider.js
@@ -1,8 +1,38 @@
 import React, { useState, useEffect } from "react";
 import Slide from "./Slide";
 import data from "../data";
+
 const Slider = () => {
-  return <div className="container slider">Slider</div>;
+  const [recensioni, setRecensioni] = useState(data);
+
+  const [active, setActive] = useState(0);
+
+  return (
+    <div className="container slider">
+      {recensioni.map((recensione, index, array) => {
+        let positionClass = "";
+
+        if (index === active) {
+          positionClass = "active";
+        } else if (
+          index + 1 === active ||
+          (active === 0 && index === recensioni.length - 1)
+        ) {
+          positionClass = "prev";
+        } else {
+          positionClass = "next";
+        }
+
+        return (
+          <Slide key={recensione.id} {...recensione} classes={positionClass} />
+        );
+      })}
+      <div className="btn-group slider-btn-group">
+        <button className="btn btn-slider prev-slider">prev</button>
+        <button className="btn btn-slider next-slider">next</button>
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
